ced9f746005471b12323d666b26dfb53be2f75ec
Hamza Bouzeboudja <hamza@educhain.io>
2025-03-28 18:21:28 +0100

    Section 03 - Pads CHallenge p02,p03

--- Section 03/44. Pads Challenge/src/App.jsx
+++ Section 03/44. Pads Challenge/src/App.jsx
@@ -3,11 +3,20 @@ import { useState } from "react"
 import Pad from "./Pad"
 
 export default function App() {
+
+
     const [padsArr, setPadsArr] = useState(pads)
 
-   
+  
+    function setToggle(id){
+        console.log("clicked")
+        console.log(id)
+    }
+
+    console.log(padsArr)
+
     const buttonElements = padsArr.map(e =>(
-        <Pad data={e}  key={e.id} />
+        <Pad toggle={setToggle} data={e}  key={e.id} />
     ))
 
     return (
--- Section 03/44. Pads Challenge/src/Pad.jsx
+++ Section 03/44. Pads Challenge/src/Pad.jsx
@@ -2,11 +2,18 @@ import { useState } from "react";
 import pads from "./pads";
 
 export default function Pad(props){
+    let [padState, setPadState] = useState(props.data.on)
+
+    function handleClick(){
+        setPadState(function(prev){
+            return !prev
+        })
+    }
 
     let style={
         backgroundColor:props.data.color
     }
     return(
-        <button style={style} className={props.data.on?"on":"off"}></button>
+        <button onClick={props.toggle} style={style} className={padState?"on":"off"}></button>
     )
 }
