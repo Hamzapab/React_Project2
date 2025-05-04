b3f79b113396ac3a33742597686fe31ae61e8ca5
Hamza Bouzeboudja <hamza@educhain.io>
2025-05-04 18:41:18 +0100

    add extra styles to the game

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,19 +1,34 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect ,useRef} from 'react'
 import { nanoid } from 'nanoid'
 import { Die } from './components/Die'
+import Confetti from 'react-confetti'
 import './index.css'
 
 
 function App() {
 
-  const [dice,setDice] = useState(getRandomDiceValue());
+  const [dice,setDice] = useState(() => getRandomDiceValue());
+  const [timeLeft, setTimeLeft] = useState(3 * 60); 
+  const [youLoose,setYouLoose] = useState(false)
+
+  const timerRef = useRef(null);
+
 
   let gameStatus;
 
+  let counterStat;
+
+  const winRef = useRef(null);
+
   if((dice.every(d => d.num === dice[0].num)) && dice.every(d=>d.isHeld)){
+
     gameStatus = true;
   }
-
+  useEffect(()=>{
+    if(gameStatus){
+      winRef.current.focus();
+    }
+  },[gameStatus])
   function getRandomDiceValue(){
      const diceArray = [];
       for(let i = 0;i < 10;i++){
@@ -28,10 +43,14 @@ function App() {
       return diceArray
    }
    function rollDice(){
-    setDice(pre=>pre.map(obj=>
-       obj.isHeld ? obj : {...obj,num:Math.ceil(Math.random() * 6)}
-    ))
-  
+    if(!gameStatus){
+      setDice(pre=>pre.map(obj=>
+        obj.isHeld ? obj : {...obj,num:Math.ceil(Math.random() * 6)}
+     ))
+    }else{
+      setDice(getRandomDiceValue())
+      setTimeLeft(3*60)
+    }
   }
   
   function hold(id) {
@@ -39,6 +58,40 @@ function App() {
       obj.id === id ? { ...obj, isHeld: !obj.isHeld } : obj
     ));
   }
+
+  useEffect(() => {
+    if(!youLoose && !gameStatus){
+      timerRef.current = setInterval(() => {
+        setTimeLeft(prev => {
+         
+          if (prev <= 1) {
+            clearInterval(timerRef.current);
+            setDice(getRandomDiceValue)
+            setYouLoose(true)
+            return 0;
+          }
+          return prev - 1;
+        });
+      }, 1000);  
+    } 
+    return () => clearInterval(timerRef.current);
+  }, [youLoose,gameStatus]);
+
+
+  
+  const formatTime = (seconds) => {
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+    return `${min}:${sec < 10 ? '0' : ''}${sec}`;
+  };
+
+
+
+  function handleTryAgain(){
+    setYouLoose(false)
+    setTimeLeft(3*60)
+  }
+
 
   const dieElem =  dice.map((value)=>
   <Die 
@@ -50,12 +103,23 @@ function App() {
   return (
     <>
      <main>
+      {youLoose && <div className='loose'>
+        You Loose click here to Play again 
+        <button onClick={handleTryAgain}>Try again</button>
+      </div>}
+      <div className='counter'  style={{ color: timeLeft < 60 ? 'red' : 'white' }}>
+      {formatTime(timeLeft)}
+      </div>
+     {gameStatus && <Confetti />}
+     <div aria-live='polite' className='sr-only'>
+      {gameStatus && <p>Congratulations you won!</p>}
+     </div>
      <h1 className="title">Tenzies</h1>
      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="die-container">
          {dieElem}
         </div>
-        <button onClick={rollDice} className='roll'>{gameStatus ? 'New game' : "Roll"}</button>
+        <button ref={winRef}  onClick={rollDice} className='roll'>{gameStatus ? 'New game' : "Roll"}</button>
      </main>
     </>
   )
--- Tenzies/src/components/Die.jsx
+++ Tenzies/src/components/Die.jsx
@@ -3,9 +3,42 @@ import React from "react";
 
 export function Die(props){
 
+    const RestpipPositions = {
+        6: [5,3,4],
+        5: [1,9,3,1],
+        4: [1,5,9,2,1],
+        3: [1,2,3,4,5,6],
+        2: [1,3,4,5,6,7,8],
+        1: [1,2,3,4,6,7,8,9],
+      };
+
+    const pipPositions = {
+        1: [5],
+        2: [1, 9],
+        3: [1, 5, 9],
+        4: [1, 3, 7, 9],
+        5: [1, 3, 5, 7, 9],
+        6: [1, 3, 4, 6, 7, 9],
+      };
+   
+      const activePositions = pipPositions[props.value];
+
+
     return(
-        <button onClick={props.onclick} data-key={props.dataKey} style={{ backgroundColor: props.isHeld ? "#59E391" : "white" }}>
-            {props.value}
+        <button 
+        onClick={props.onclick} 
+        data-key={props.dataKey} 
+        aria-label={`Die with vale ${props.value}`}
+        aria-pressed={props.isHeld}
+        style={{ backgroundColor: props.isHeld ? "#59E391" : "white" 
+        }}>
+            <div className="pips">
+                 {[...Array(9)].map((_, i) => (
+                    <div key={i} className="cell">
+                        {activePositions.includes(i + 1) && <div className="pipActive"></div>}
+                    </div>
+                ))}
+            </div>
         </button>
     )
 }
